38726b0d24b4f63dd189066a5f314e2fa13b2830
Sigge <sigge@ubuntu.ubuntu-domain>
2009-10-27 15:58:55 +0100

    Jag har fixat så man kan skicka hur mycket som heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeelst?!

--- Server - Client/socketclient.py
+++ Server - Client/socketclient.py
@@ -1,6 +1,5 @@
 # Echo client program
-import socket, ssl, pprint# require a certificate from the server
-
+import socket, ssl, pprint # require a certificate from the server
 HOST = '127.0.1.1'    # The remote host
 PORT = 443              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,22 +7,20 @@ ssl_sock = ssl.wrap_socket(s,
                            ca_certs="ca/cacert.pem",
                            cert_reqs=ssl.CERT_REQUIRED)
 ssl_sock.connect((HOST, PORT))
-
-#s.connect((HOST, PORT))
 f = open('text.txt', 'r')
 polentext = f.read()
 f.close()
 print repr(ssl_sock.getpeername())
 print ssl_sock.cipher()
 print pprint.pformat(ssl_sock.getpeercert())
-ssl_sock.write(polentext)
-#s.send(polentext)
-print "2"
-data = ssl_sock.read()
-#s.recv(10000000)
-print "1"
+totalsent =  0
+while totalsent < polentext.__len__():
+    sent = ssl_sock.write(polentext[totalsent:totalsent + 1000])
+    if sent == 0:
+        raise RuntimeError, "socket connection broken"
+    totalsent = totalsent + sent
 ssl_sock.close()
-print 'Received', repr(data)
-f = open('result.txt', 'w')
-f.write(repr(data))
-f.close()
+#print 'Received', repr(data)
+#f = open('result.txt', 'w')
+#f.write(repr(data))
+#f.close()
--- Server - Client/socketserver.py
+++ Server - Client/socketserver.py
@@ -1,6 +1,6 @@
 import socket, ssl
-HOST = '127.0.1.1'                 # Symbolic name meaning all available interfaces
-PORT = 443              # Arbitrary non-privileged port
+HOST = '127.0.1.1' # Symbolic name meaning all available interfaces
+PORT = 443 # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print HOST
 print PORT
@@ -14,16 +14,14 @@ connstream = ssl.wrap_socket(conn,
                                 keyfile="ca/key.pem",
                                 ssl_version=ssl.PROTOCOL_SSLv23)
 print 'Connected by', addr
-#while 1:
-#    data = conn.recv(10000000)
-#    if not data: break
-#    conn.send(data)
-# null data means the client is finished with us
-print socket.gethostbyname(socket.gethostname()) 
-while True:
-    data = connstream.read()
-    if not data: break
-    print "Hejsan"
-    connstream.write(data) 
-# finished with client
+print socket.gethostbyname(socket.gethostname())
+utdata = ""
+while True: 
+    data = connstream.read(8192)
+    if not data: break 
+    utdata = utdata + str(data)
+print "Servern har denna utdata", utdata
 connstream.close()
+f = open('result.txt', 'w')
+f.write(utdata)
+f.close
