92bfcc44baf57ea2144756b08d344a43d3ecee69
lytharn <lytharn@mewx.(none)>
2009-10-27 13:22:34 +0100

    Fixat vägar i ssl

--- Server - Client/socketclient.py
+++ Server - Client/socketclient.py
@@ -1,16 +1,28 @@
 # Echo client program
-import socket
+import socket, ssl, pprint# require a certificate from the server
 
-HOST = '192.168.1.49'    # The remote host
+HOST = '192.168.1.36'    # The remote host
 PORT = 443              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-f = open('/home/lytharn/Python/text.txt', 'r')
+ssl_sock = ssl.wrap_socket(s,
+                           ca_certs="ca/cacert.pem",
+                           cert_reqs=ssl.CERT_REQUIRED)
+ssl_sock.connect((HOST, PORT))
+
+#s.connect((HOST, PORT))
+f = open('text.txt', 'r')
 polentext = f.read()
 f.close()
-s.send(polentext)
-data = s.recv(10000000)
-s.close()
+print repr(ssl_sock.getpeername())
+print ssl_sock.cipher()
+print pprint.pformat(ssl_sock.getpeercert())
+ssl_sock.write(polentext)
+#s.send(polentext)
+print "2"
+data = ssl_sock.read()
+#s.recv(10000000)
+print "1"
+ssl_sock.close()
 print 'Received', repr(data)
 f = open('result.txt', 'w')
 f.write(repr(data))
--- Server - Client/socketserver.py
+++ Server - Client/socketserver.py
@@ -1,14 +1,28 @@
-import socket
-HOST = '192.168.1.49'                 # Symbolic name meaning all available interfaces
+import socket, ssl
+HOST = '192.168.1.36'                 # Symbolic name meaning all available interfaces
 PORT = 443              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print HOST
+print PORT
 s.bind((HOST, PORT))
 print 'Server is listening...'
 s.listen(1)
 conn, addr = s.accept()
+connstream = ssl.wrap_socket(conn, 
+                                server_side=True,
+                                certfile="ca/cert.pem",
+                                keyfile="ca/key.pem",
+                                ssl_version=ssl.PROTOCOL_SSLv23)
 print 'Connected by', addr
-while 1:
-    data = conn.recv(10000000)
+#while 1:
+#    data = conn.recv(10000000)
+#    if not data: break
+#    conn.send(data)
+# null data means the client is finished with us
+while True:
+    data = connstream.read()
     if not data: break
-    conn.send(data)
-conn.close()
+    print "Hejsan"
+    connstream.write(data) 
+# finished with client
+connstream.close()
