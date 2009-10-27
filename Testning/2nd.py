69021ae7f492e9fd86d38dde418240f7d8f590d5
Jonas <jonas@ubuntu.ubuntu-domain>
2009-10-27 15:36:18 +0100

    en ny testning

--- Testning/2nd.py
+++ Testning/2nd.py
@@ -9,17 +9,21 @@ class helloWorld(hildon.Program):
         self.window.connect("destroy", gtk.main_quit)
         self.add_window(self.window)
         
-        label = gtk.Label("hello Jonas")
-        self.window.add(label)
-        label.show()
+        self.box1 = gtk.VBox(False, 0)
+        self.window.add(self.box1)
         
-        button = gtk.Button("Widget")
-        self.window.add(button)
-        button.connect("clicked",self.whoop(label))
-        button.show()
+        self.label = gtk.Label("hello Jonas")
+        self.button = gtk.Button("Widget")
+        self.button.connect("clicked",self.whoop)
+        self.box1.pack_start(self.button, True, True, 0)
+        self.box1.pack_start(self.label, True, True, 0)
+        self.button.show()
+        self.label.show()
+        
 
-    def whoop(self):
-        self.set_label("tryckt")
+
+    def whoop(self, label):
+        self.label.set_label("tryckt")
 
     def run(self):
         self.window.show_all()
