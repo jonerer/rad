43210048b6720098286383fc6a4c3a70285c10e7
Sigge <sigge@ubuntu.ubuntu-domain>
2009-10-28 11:59:57 +0100

    Jag har gjort en haxxor flaxxor roxxor proxxor server

--- Server - Client/socketserver.py
+++ Server - Client/socketserver.py
@@ -1,27 +1,41 @@
-import socket, ssl
-HOST = '127.0.1.1' # Symbolic name meaning all available interfaces
-PORT = 443 # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print HOST
-print PORT
-s.bind((HOST, PORT))
-print 'Server is listening...'
-s.listen(1)
-conn, addr = s.accept()
-connstream = ssl.wrap_socket(conn, 
-                                server_side=True,
-                                certfile="ca/cert.pem",
-                                keyfile="ca/key.pem",
-                                ssl_version=ssl.PROTOCOL_SSLv23)
-print 'Connected by', addr
-print socket.gethostbyname(socket.gethostname())
-utdata = ""
-while True: 
-    data = connstream.read(8192)
-    if not data: break 
-    utdata = utdata + str(data)
-print "Servern har denna utdata", utdata
-connstream.close()
-f = open('result.txt', 'w')
-f.write(utdata)
-f.close
+# -*- coding: utf-8 -*
+import socket, ssl, threading
+
+class SocketServer(object):
+
+    certpath = "ca/cert.pem"
+    keypath = "ca/key.pem"
+
+    def __init__(self, HOST='127.0.1.1', PORT = 443):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST,PORT))
+        print 'Server is listening...'
+        s.listen(5)
+        self.socketclienttable = {}
+        self.clientid = 0
+        while True:
+        # Waiting for new client to accept
+            conn, addr = s.accept()
+            sslsocket = self.wrapsocket(conn,addr)
+            self.socketclienttable[self.clientid] = sslsocket
+            self.clientid = self.clientid + 1
+            threading.Thread(target=self.write,args=(self.clientid-1,)).start()
+
+    def wrapsocket(self, socket,address):
+        sslsocket = ssl.wrap_socket(socket, server_side=True, certfile=self.certpath, keyfile=self.keypath, ssl_version=ssl.PROTOCOL_SSLv23)
+        print 'Connected by', address
+        return sslsocket
+    
+    def write(self, id):
+        socket = self.socketclienttable[id]
+        utdata = ""
+        while True: 
+            data = socket.read(8192)
+            if not data: break 
+            utdata = utdata + str(data)
+        socket.close()
+        f = open('Data sent/result' + repr(id) + ".txt", 'w')
+        f.write(utdata)
+        f.close
+
+socketArray = list()
