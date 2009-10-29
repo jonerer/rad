494c80c6d3dd242cf4f3d1ee11911ed8420af76a
Sigge <sigge@ubuntu.ubuntu-domain>
2009-10-29 15:37:01 +0100

    Server o client lol

--- Server - Client/socketclient.py
+++ Server - Client/socketclient.py
@@ -1,26 +1,58 @@
+# -*- coding: utf-8 -*
 # Echo client program
-import socket, ssl, pprint # require a certificate from the server
-HOST = '127.0.1.1'    # The remote host
-PORT = 443              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock = ssl.wrap_socket(s,
-                           ca_certs="ca/cacert.pem",
-                           cert_reqs=ssl.CERT_REQUIRED)
-ssl_sock.connect((HOST, PORT))
-f = open('text.txt', 'r')
-polentext = f.read()
-f.close()
-print repr(ssl_sock.getpeername())
-print ssl_sock.cipher()
-print pprint.pformat(ssl_sock.getpeercert())
-totalsent =  0
-while totalsent < polentext.__len__():
-    sent = ssl_sock.write(polentext[totalsent:totalsent + 1000])
-    if sent == 0:
-        raise RuntimeError, "socket connection broken"
-    totalsent = totalsent + sent
-ssl_sock.close()
-#print 'Received', repr(data)
-#f = open('result.txt', 'w')
-#f.write(repr(data))
-#f.close()
+import socket, threading # require a certificate from the server
+from OpenSSL.SSL import Context, Connection, TLSv1_METHOD
+
+class SocketClient(object):
+    """This class talks to the server
+    """
+
+    cacertpath = "ca/cacert.pem"
+    BUFF = 8192
+
+    def __init__(self,HOST='127.0.1.1', PORT = 443):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        context = Context(TLSv1_METHOD)
+        context.use_certificate_file(self.cacertpath)
+        self.sslsocket = Connection(context,s)
+        self.sslsocket.connect((HOST,PORT))
+        threading.Thread(target=self.receive).start()
+        print "Yeah, tråden fungerar"
+        target=self.sendinput()
+
+    def send(self,str):
+        self.sslsocket.write("start")
+        totalsent =  0
+        while totalsent < str.__len__():
+            sent = self.sslsocket.write(str[totalsent:])
+            if sent == 0:
+                raise RuntimeError, "socket connection broken"
+            totalsent = totalsent + sent
+        self.sslsocket.write("end")
+    
+    def sendinput(self):
+        while True:
+            input = raw_input()
+            if input == "exit": 
+                self.sslsocket.close()
+            self.send(input) 
+
+    def receive(self):
+        output = ""
+        print "Du är i receive"
+        try:
+            while True:
+                data = self.sslsocket.recv(self.BUFF)
+                if data == "start":
+                    while True:
+                        data = self.sslsocket.recv(self.BUFF)
+                        if data == "end":
+                            print output
+                            output = ""
+                            break
+                        output = output + data
+        except KeyboardInterrupt:
+            self.sslsocket.close()
+            exit()
+
+hejsan = SocketClient()
--- Server - Client/socketserver.py
+++ Server - Client/socketserver.py
@@ -1,41 +1,72 @@
 # -*- coding: utf-8 -*
-import socket, ssl, threading
+import socket, threading
+from OpenSSL.SSL import Context, Connection, TLSv1_METHOD
 
 class SocketServer(object):
-
+    """This class is setting up a server that is listining to port 443
+    """
     certpath = "ca/cert.pem"
     keypath = "ca/key.pem"
+    BUFF = 8192
 
     def __init__(self, HOST='127.0.1.1', PORT = 443):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST,PORT))
+        context = Context(TLSv1_METHOD)
+        context.use_certificate_file((self.certpath))
+        context.use_privatekey_file(self.keypath) 
+        conn = Connection(context,s)
+        conn.bind((HOST,PORT))
         print 'Server is listening...'
-        s.listen(5)
+        conn.listen(5)
         self.socketclienttable = {}
         self.clientid = 0
         while True:
         # Waiting for new client to accept
-            conn, addr = s.accept()
-            sslsocket = self.wrapsocket(conn,addr)
+            sslsocket, addr = conn.accept()
+            print 'Connected to', addr
             self.socketclienttable[self.clientid] = sslsocket
             self.clientid = self.clientid + 1
-            threading.Thread(target=self.write,args=(self.clientid-1,)).start()
+            threading.Thread(target=self.receive,args=(self.clientid-1,)).start()
+            while True:
+                    hejsan = self.sendinput()
+                    self.send(sslsocket,hejsan)
 
-    def wrapsocket(self, socket,address):
-        sslsocket = ssl.wrap_socket(socket, server_side=True, certfile=self.certpath, keyfile=self.keypath, ssl_version=ssl.PROTOCOL_SSLv23)
-        print 'Connected by', address
-        return sslsocket
+    def receive(self, id):
+        sslsocket = self.socketclienttable[id]
+        output = ""
+        try: 
+            while True:
+                data = sslsocket.recv(self.BUFF)
+                if data == "start":
+                    while True:
+                        data = sslsocket.recv(self.BUFF)
+                        if data == "end":
+                            if output == "hejsan":
+                                print "lol"
+                                #hejsan = self.sendinput()
+                                self.send(sslsocket,"hejsan") 
+                                #threading.Thread(target=self.send,args=((sslsocket, "hi client"))).start()
+                            print output
+                            output = ""
+                            break
+                        output = output + data
+        except KeyboardInterrupt:
+            sslsocket.close()
+            exit()
+
+    def send(self, socket, str):
+        socket.write("start")
+        totalsent =  0
+        while totalsent < str.__len__():
+            sent = socket.write(str[totalsent:])
+            if sent == 0:
+                raise RuntimeError, "socket connection broken"
+            totalsent = totalsent + sent
+        socket.write("end")
+
     
-    def write(self, id):
-        socket = self.socketclienttable[id]
-        utdata = ""
-        while True: 
-            data = socket.read(8192)
-            if not data: break 
-            utdata = utdata + str(data)
-        socket.close()
-        f = open('Data sent/result' + repr(id) + ".txt", 'w')
-        f.write(utdata)
-        f.close
+    def sendinput(self):
+        input = raw_input()
+        return input
 
-socketArray = list()
+tjabba = SocketServer()
