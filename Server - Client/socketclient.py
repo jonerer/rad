1df53ea16305bc81b5c481d85fb4a6f7a87f6023
Sigge <sigge@ubuntu.ubuntu-domain>
2009-11-04 09:56:16 +0100

    Fuck yeaFuck yeahh

--- Server - Client/socketclient.py
+++ Server - Client/socketclient.py
@@ -10,13 +10,15 @@ class SocketClient(object):
     cacertpath = "ca/cacert.pem"
     BUFF = 8192
 
-    def __init__(self,HOST='130.236.219.241', PORT = 443):
+    def __init__(self,HOST='localhost', PORT = 443):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         context = Context(TLSv1_METHOD)
         context.use_certificate_file(self.cacertpath)
+        context.set_timeout(2)
         self.sslsocket = Connection(context,s)
         self.sslsocket.connect((HOST,PORT))
         #starting a thread that listen to what server sends which the clients need to be able to send and recive data at the same time
+        self.connection_killed = False
         threading.Thread(target=self.receive).start()
         print "Yeah, tråden fungerar"
         target=self.sendinput()
@@ -39,18 +41,31 @@ class SocketClient(object):
                 input = raw_input()
                 self.send(input)
         except KeyboardInterrupt:
-            print "You are now in interrupt root"
-            self.sslsocket.close()
+            print "fuck this shit"
+            self.sslsocket.write("fuckthisshit")
+            self.connection_killed = True
 
     #getting data from server
     def receive(self):
         output = ""
         print "Du är i receive"
-        while True:
+        buff = ""
+        while not self.connection_killed:
             data = self.sslsocket.recv(self.BUFF)
+            buff = data
+            if buff == "fuckthisshit":
+                self.sslsocket.close()
+                break
+            if data == "fuckthisshitservergoingdown":
+                self.sslsocket.write("fuckthisshit")
+                self.sslsocket.close()
+                break
+            print data
             if data == "start":
-                while True:
+                while not self.connection_killed:
+                    print "tjock?"
                     data = self.sslsocket.recv(self.BUFF)
+                    print "nah, det släppte"
                     if data == "end":
                         print output
                         output = ""
