a2f60be216b79346486557c6f866992adfc64dd8
Uffe <uffe@ubuntu.ubuntu-domain>
2009-11-24 10:55:36 +0100

    fixat

--- tester/camera/GTK_Main.py
+++ tester/camera/GTK_Main.py
@@ -29,11 +29,11 @@ class GTK_Main:
 		hbox.pack_start(self.button2, False)
 		hbox.add(gtk.Label())
 		window.show_all()
-
-		options = "v4l2src ! video/x-raw-yuv, width=320, height=240, framerate=8/1 ! hantro4200enc ! udpsink host=130.236.218.251 port=5435"
+		#Skickar
+		options = "v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.122 port=5435"
 		self.player = gst.parse_launch ( options )
-
-		options2 = "udpsrc port=5435 caps=application/x-rtp,clock-rate=90000 ! hantro4100dec ! xvimagesink"
+		#visar
+		options2 = "udpsrc port=5434 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink"
 
 		self.player1 = gst.parse_launch( options2 )
 
