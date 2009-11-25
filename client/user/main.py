90a5b6a59a6ea13f9f51efbdcdf19041a7c724e5
Jonas <jonas@ubuntu.ubuntu-domain>
2009-11-25 14:26:14 +0100

    gps mer

--- client/user/main.py
+++ client/user/main.py
@@ -5,24 +5,29 @@ from shared import rpc
  
 def main():
     rpc.set_name("rpcsender")
-    context = gpsbt.start()
+    #context = gpsbt.start()
     # ensure that GPS device is ready to connect and to receive commands
-    time.sleep(2)
-    gpsdevice = gpsbt.gps()
+    #time.sleep(2)
+    #gpsdevice = gpsbt.gps()
     while True:
         # read 3 times and show information
-        for a in range(4):
-            gpsdevice.get_fix()
-            time.sleep(15)
-        while gpsdevice.get_position() == (0,0):
-            time.sleep(1)  
-        lon, lat = gpsdevice.get_position()
+        #for a in range(4):
+            #gpsdevice.get_fix()
+            #time.sleep(15)
+        #while gpsdevice.get_position() == (0,0):
+            #time.sleep(1)  
+        #lon, lat = gpsdevice.get_position()
+        time.sleep(5)
+        lon = 15.5629
+        lat = 58.4093
         print lon
         print lat
         print "Nu skickar jag koordinater"
-        print rpc.send("main", "ping_with_coordinates", lon=lat, lat=lon)
+        print rpc.send("main", "ping_with_coordinates", lon=lon, lat=lat)
         print "skickat koord, updaterar map"
         print rpc.send("main", "update_map")
+        lon = lon + 00.0010
+        lat = lat + 00.0010
     #stop gps devices
     gpsbt.stop(context)
 
