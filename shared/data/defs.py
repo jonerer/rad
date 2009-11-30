e5421faabccbe2fe0cd63a55d8c235f35a621f33
Filip <filho270@student.liu.se>
2009-11-30 15:47:03 +0000

    oh yea

--- shared/data/defs.py
+++ shared/data/defs.py
@@ -9,8 +9,7 @@ from sqlalchemy.orm import relation, backref
 Base = declarative_base()
 
 # lägg allt som ska synas utåt här: (dessa får man från from shared.data.defs import *
-__all__ = ['UnitType', 'Unit', 'User', 'Mission', 'Document', 
-        'AlarmType', 'Alarm', 'POI', 'POIType']
+__all__ = ['UnitType', 'Unit', 'User', 'Mission', 'Document', 'POI', 'POIType']
 
 class UnitType(Base):
     __tablename__ = "unit_types"
@@ -22,15 +21,15 @@ class UnitType(Base):
         self.name = name
         self.image = image
 
-class AlarmType(Base):
-    __tablename__ = "alarm_types"
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode)
-    image = Column(String)
+#class AlarmType(Base):
+    #__tablename__ = "alarm_types"
+    #id = Column(Integer, primary_key=True)
+    #name = Column(Unicode)
+    #image = Column(String)
 
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
+    #def __init__(self, name, image):
+        #self.name = name
+        #self.image = image
 
 class POIType(Base):
     __tablename__ = "poi_types"
@@ -47,41 +46,46 @@ class POI(Base):
     coordx = Column(Float)
     coordy = Column(Float)
     id = Column(Integer, primary_key=True)
-    uid = Column(Integer)
     name = Column(Unicode)
-    sub_type = Column(Unicode) 
-    timestamp = Column(DateTime)
-    type_id = Column(Integer, ForeignKey("poi_types.id"))
+    time_created = Column(Integer)
+    time_changed = Column(Integer)
     type = relation(POIType, backref=backref("poi", order_by=id)) 
+    subtype = Column(Unicode) 
     
-    def __init__(self, coordx, coordy, uid, name, type, timestamp):
+    uid = Column(Integer) #Internt ID?
+    type_id = Column(Integer, ForeignKey("poi_types.id"))
+    
+    
+    def __init__(self, coordx, coordy, uid, name, type, subtype, time_created, time_changed):
         from shared.data import get_session, create_tables
         session = get_session()
         self.coordx = coordx
         self.coordy = coordy
         self.uid = uid
         self.type = type
+        self.subtype = subtype
         self.name = name
-        self.timestamp = timestamp
+        self.time_created = time_created
+        self.time_changed = time_changed
+        
+#class Alarm(Base):
+    #__tablename__ = "alarm"
+    #id = Column(Integer, primary_key=True)
+    #name = Column(Unicode)
+    #type_id = Column(Integer, ForeignKey("alarm_types.id"))
+    #type = relation(AlarmType, backref=backref("alarm", order_by=id))
+    #extrainfo = Column(Unicode)
+    #coordx = Column(Float)
+    #coordy = Column(Float)
+    #timestamp = Column(DateTime)
 
-class Alarm(Base):
-    __tablename__ = "alarm"
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode)
-    type_id = Column(Integer, ForeignKey("alarm_types.id"))
-    type = relation(AlarmType, backref=backref("alarm", order_by=id))
-    extrainfo = Column(Unicode)
-    coordx = Column(Float)
-    coordy = Column(Float)
-    timestamp = Column(DateTime)
-
-    def __init__(self, coordx, coordy, id, name, sub_type, timestamp):
-        self.coordx = coordx
-        self.coordy = coordy
-        self.id = id
-        self.name = name
-        self.sub_type = sub_type
-        self.timestamp = timestamp
+    #def __init__(self, coordx, coordy, id, name, sub_type, timestamp):
+        #self.coordx = coordx
+        #self.coordy = coordy
+        #self.id = id
+        #self.name = name
+        #self.sub_type = sub_type
+        #self.timestamp = timestamp
 
 
 class Unit(Base):
@@ -90,9 +94,11 @@ class Unit(Base):
     name = Column(Unicode)
     coordx = Column(Float) # longitude
     coordy = Column(Float) # latitude
-    type_id = Column(Integer, ForeignKey("unit_types.id"))
     type = relation(UnitType, backref=backref("units", order_by=id))
+    time_changed = Column(Integer)
+    
     is_self = Column(Boolean)
+    type_id = Column(Integer, ForeignKey("unit_types.id"))
 
     def get_image(self):
         if not self.is_self:
@@ -100,11 +106,12 @@ class Unit(Base):
         else:
             return "JonasInGlases.png"
 
-    def __init__(self, name, type, coordx=None, coordy=None, is_self=False):
+    def __init__(self, name, type, coordx, coordy, time_changed, is_self):
         self.name = name
         self.type = type
         self.coordx = coordx
         self.coordy = coordy
+        self.time_changed = time_changed
         self.is_self = is_self
         
     def __repr__(self):
@@ -127,11 +134,16 @@ class User(Base):
 class Mission(Base):
     __tablename__ = "mission"
     id = Column(Integer, primary_key=True)
+    time_created = Column(Integer)
+    time_changed = Column(Integer)
     name = Column(Unicode)
     coordx = Column(Float)
     coordy = Column(Float)
-    info = Column(Unicode)
-    prio = Column(Integer)
+    status = Column(Unicode)
+    desc = Column(Unicode)
+    
+    # poi_ids = ?? RELATION ??
+    # unit_ids = ?
     
     def __repr__(self):
         return "Entity '%s' of type %s" % (self.id, self.name)
