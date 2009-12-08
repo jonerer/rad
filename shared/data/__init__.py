686459e2f91c6514223f26aade4f1d0d0fdbad4e
Jon <jon@ubuntu.ubuntu-domain>
2009-12-08 11:11:54 +0100

    azzzz

--- shared/data/__init__.py
+++ shared/data/__init__.py
@@ -15,7 +15,9 @@ def get_session(db="sqlite:///client/db.db", echo=False):
     if _dbfile is None or _dbfile != db:
         # new db
         _engine = create_engine(db, echo=echo)
-        _session = sessionmaker(bind=_engine, expire_on_commit=False)()
+        # worksade inte: :S
+        #_session = sessionmaker(bind=_engine, expire_on_commit=False)()
+        _session = sessionmaker(bind=_engine)()
     return _session
 
 def create_tables():
